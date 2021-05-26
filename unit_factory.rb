74be5c1b975d43ac6b1081db0546ddbef47a6716
FerretCode <674313@salemkeizer.org>
2021-05-26 22:13:51 +0000

    add more unit classes to unit_factory.rb

--- unit_factory.rb
+++ unit_factory.rb
@@ -1,3 +1,17 @@
+class TankUnit
+    avaibale_types = {
+        "france": {
+            "ft": 80
+            "r35": 90
+            "char d1": 200
+            "amc 35": 220
+            "char 2c": 500
+            "black prince": 550
+            "fcm f1": 700
+        }
+    }
+end
+
 class FactoryUnit
     avaibale_types = {
         "airport": 2400
@@ -7,6 +21,27 @@ class FactoryUnit
     }
 
     def initialize(cost, nationality, type) 
+        @cost = cost
+        @nationality = nationality
+        @type = type
+    end
+
+    def getVariables
+        return [@cost, @nationality, @type]
+    end
+end
+
+class InfantryUnit
+    avaibale_types = {
+        "assault": 35
+        "atr": 45
+        "flamethrower": 35
+        "smg": 15
+        "sniper": 30
+        "rifleman": 10
+    }
+
+    def initialize(cost, nationality, type)
         @cost = cost
         @nationality = nationality
         @type = type
